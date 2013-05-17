65109684939537dce77dfb5a7423318b295fb08b
Matthew Dettinger <mcdett@gmail.com>
2013-05-17 09:59:27 -0500

    Adding key file for easier key maintaince on testing systems

--- lookup_stream.php
+++ lookup_stream.php
@@ -7,16 +7,13 @@ All rights reserved.
 
 */
 require("phpgsb.class.php");
-$phpgsb = new phpGSB("DATABASE_NAME","DATABASE_USERNAME","DATABASE_PASSWORD");
-//Obtain an API key from: http://code.google.com/apis/safebrowsing/key_signup.html
-$phpgsb->apikey = "API_KEY_HERE";
+require("api_keys.php");
+
 $phpgsb->usinglists = array('googpub-phish-shavar','goog-malware-shavar');
 //Should return false (not phishing or malware)
 var_dump($phpgsb->doLookup('http://www.google.com'));
 //Should return true, malicious URL
 var_dump($phpgsb->doLookup('http://www.gumblar.cn'));
 $phpgsb->close();
-
-
 
 ?>
